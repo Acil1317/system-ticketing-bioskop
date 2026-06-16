bfb01e6e437004e3c123b4b9259e0c65d995f521
Maftuh Faisal Jauhari <maftuhfaisal174@gmail.com>
2026-06-16 11:40:20 +0700

    upload folder record_data

---
+++ pembatalan.sql
@@ -0,0 +1,14 @@
+-- --------------------------------------------------
+-- Mengisi Data Tabel Pembatalan (10 Data)
+-- --------------------------------------------------
+INSERT INTO pembatalan (pembatalan_id, pemesanan_id, waktu_pembatalan, alasan_pembatalan, jumlah_refund) VALUES
+('CAN001', 'PMN005', '2026-01-15 10:30:00', 'Ada urusan mendadak', 150000),
+('CAN002', 'PMN012', '2026-01-20 14:15:00', 'Salah pilih jadwal', 100000),
+('CAN003', 'PMN024', '2026-02-12 09:45:00', 'Kendaraan mogok', 50000),
+('CAN004', 'PMN034', '2026-03-05 16:20:00', 'Kurang sehat', 50000),
+('CAN005', 'PMN043', '2026-04-02 11:00:00', 'Ada rapat kantor', 50000),
+('CAN006', 'PMN055', '2026-04-25 08:30:00', 'Keluarga sakit', 50000),
+('CAN007', 'PMN063', '2026-05-10 13:50:00', 'Salah pesan studio', 50000),
+('CAN008', 'PMN073', '2026-05-22 15:10:00', 'Cuaca buruk/hujan', 150000),
+('CAN009', 'PMN084', '2026-06-05 10:05:00', 'Jadwal bentrok', 150000),
+('CAN010', 'PMN095', '2026-06-12 12:40:00', 'Tidak bisa hadir', 100000);
