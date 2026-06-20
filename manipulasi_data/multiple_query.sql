de7d8e7613717464b391401d64ebf5a79f18c11e
Maftuh Faisal Jauhari <maftuhfaisal174@gmail.com>
2026-06-20 21:38:32 +0700

    Update multiple_query.sql

--- manipulasi_data/multiple_query.sql
+++ manipulasi_data/multiple_query.sql
@@ -1,20 +1,36 @@
 -- --------------------------------------------------
--- INNER JOIN (Melihat daftar ID pelanggan, nama mereka, dan nama studio yang mereka pesan)
+-- INNER JOIN Analitik (Menampilkan Total Tiket per Pelanggan dengan Nama Lengkap & Judul Film)
 -- --------------------------------------------------
-SELECT DISTINCT p.pelanggan_id, scr.film_name, st.nama AS nama_studio
+SELECT 
+    p.pelanggan_id, 
+    CONCAT_WS(' ', p.nama_depan, p.nama_belakang) AS nama_lengkap, 
+    f.judul AS judul_film, 
+    st.nama AS nama_studio,
+    COUNT(res.seat_id) AS total_tiket_dipesan
 FROM pelanggan p
 INNER JOIN pemesanan pem ON p.pelanggan_id = pem.pelanggan_id
 INNER JOIN reservasi res ON pem.pemesanan_id = res.pemesanan_id
 INNER JOIN seats s ON s.seat_id = res.seat_id
 INNER JOIN studio st ON st.studio_id = s.studio_id
 INNER JOIN screenings scr ON scr.screening_id = pem.screening_id
-WHERE st.nama = 'Imax A'
-ORDER BY p.pelanggan_id;
+INNER JOIN films f ON scr.film_id = f.film_id
+WHERE st.nama = 'Regular B'
+GROUP BY 
+    p.pelanggan_id, 
+    p.nama_depan,     -- Masuk GROUP BY karena bagian dari nama_lengkap
+    p.nama_belakang,  -- Masuk GROUP BY karena bagian dari nama_lengkap
+    f.judul, 
+    st.nama
+ORDER BY total_tiket_dipesan DESC;
 
 -- --------------------------------------------------
--- LEFT JOIN 
+-- LEFT JOIN: Menghitung total tayang per film (Termasuk film yang belum pernah tayang)
 -- --------------------------------------------------
-SELECT p.pelanggan_id, COUNT(pem.pemesanan_id) AS total_pemesanan
-FROM pelanggan p
-LEFT JOIN pemesanan pem ON p.pelanggan_id = pem.pelanggan_id
-GROUP BY p.pelanggan_id;
+SELECT 
+    f.film_id, 
+    f.judul AS judul_film, 
+    COUNT(s.screening_id) AS total_jadwal_tayang
+FROM films f
+LEFT JOIN screenings s ON f.film_id = s.film_id
+GROUP BY f.film_id, f.judul
+ORDER BY total_jadwal_tayang DESC;
